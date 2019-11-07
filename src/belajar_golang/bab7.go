e7149b31ee58b8c3eb7bde805bf6201306fbaff6
AlMishbah <roihanmisbahulanam140@gmail.com>
2019-11-07 14:19:49 +0700

    Pemanggilan Variabel

---
+++ src/belajar_golang/bab7.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var firstName string = "Roihan"
+
+	var lastName string 
+	lastName = "Mishbahul Anam"
+
+	fmt.Printf("Hallo %s %s!\n", firstName, lastName)
+}
