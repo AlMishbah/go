ddcb50c37800cd09602797be1027b87346167e16
AlMishbah <roihanmisbahulanam140@gmail.com>
2019-11-12 14:28:12 +0700

    Constanta

---
+++ src/belajar_golang/bab9.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const firstName string = "Roihan"
+	fmt.Print("Hallo ", firstName, "!\n")
+
+	const lastName = " Mishabhul Anam"
+	fmt.Print("Nice to meet you ", firstName, lastName, "!\n")
+}
